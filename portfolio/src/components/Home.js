8905f33d0d7e226a30f81512151dc30197e50254
kailaho <kaho3670@colorado.edu>
2024-01-25 09:24:04 -0700

    fixed formatting issues

--- portfolio/src/components/Home.js
+++ portfolio/src/components/Home.js
@@ -22,43 +22,42 @@ const Home = () => {
         </div>
 
         <ScrollButton/>
-        
-        <div id="columnWrapper">
-            <div id="titles">
-                <TitleAnimation text={"UX/UI Design"}/>
-                <TitleAnimation text={"Web Development"}/>
-                <TitleAnimation text={"Product Design"}/>
-            </div>
 
-            <div id ="CardContainer">
-            <HomeProjectCard
+        <div id="CardContainer">
+            <div className ="titleCardButton">
+                <TitleAnimation className="title" text={"UX/UI Design"}/>
+                <HomeProjectCard
                 image ={hikeImg}
                 title = {"PortfoliPro ★"}
                 category={"UX/UI Design"}
                 description = {"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}
-            />
-            <HomeProjectCard
+                />
+                <SeeMoreUXUI/>
+            </div>
+
+            <div className ="titleCardButton">
+                <TitleAnimation className="title" text={"Web Development"}/>
+                <HomeProjectCard
                 image ={formImg}
                 title = {"2D to 3D Dinosaur ★"}
                 category ={"Product Design"}
                 description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}
-            />
-            <HomeProjectCard 
+                />
+               <SeeMoreWebDev/>
+            </div>
+
+            <div className="titleCardButton">
+                <TitleAnimation className="title" text={"Product Design"}/>
+                <HomeProjectCard 
                 image = {matchaImg}
                 title= {"Matcha Tea Packaging Design ★"}
                 category = {"Product Design, Graphic Design"}
                 description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}
-            />
-           
-            </div>
-
-            <div id="seeMoreContainer">
-                <SeeMoreUXUI/>
-                <SeeMoreWebDev/>
+                />
                 <SeeMoreProducts/>
             </div>
         </div>
-        
+               
 
         </div>
         </>
