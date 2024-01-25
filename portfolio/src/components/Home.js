3addd8915355c4937ca5f0457d96022bc259ab70
kailaho <kaho3670@colorado.edu>
2024-01-24 20:55:49 -0700

    original home screen: gradient background, buttons, formmatted

--- portfolio/src/components/Home.js
+++ portfolio/src/components/Home.js
@@ -5,7 +5,8 @@ import HomeAnimation from './HomeAnimation.js';
 import hikeImg from '../img/hike.jpeg';
 import formImg from '../img/formImg.png';
 import matchaImg from '../img/matcha.png';
-
+import ScrollButton from './ScrollButton.js';
+import TitleAnimation from './TitleAnimation.js';
 
 const Home = () => {
     return(
@@ -16,32 +17,39 @@ const Home = () => {
         <div>
         <HomeAnimation/>
         </div>
+
+        <ScrollButton/>
         
-        <div id = "column2">
-            <h1>Explore Projects</h1>
+        <div id="columnWrapper">
+            <div id="titles">
+                <TitleAnimation text={"UX/UI Design"}/>
+                <TitleAnimation text={"Web Development"}/>
+                <TitleAnimation text={"Product Design"}/>
+            </div>
+
             <div id ="CardContainer">
             <HomeProjectCard
                 image ={hikeImg}
-                title = {"Test title"}
-                category={"Test"}
-                description = {"Example description of a project"}
+                title = {"PortfoliPro ★"}
+                category={"UX/UI Design"}
+                description = {"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}
             />
-
             <HomeProjectCard
                 image ={formImg}
-                title = {"2D to 3D Dinosaur"}
+                title = {"2D to 3D Dinosaur ★"}
                 category ={"Product Design"}
-                description={"Fill in description here"}
+                description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}
             />
-
             <HomeProjectCard 
                 image = {matchaImg}
-                title= {"Matcha Tea Packaging Design"}
+                title= {"Matcha Tea Packaging Design ★"}
                 category = {"Product Design, Graphic Design"}
-                description={"Fill in description here"}
+                description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}
             />
+           
             </div>
         </div>
+        
 
         </div>
         </>
---
+++ portfolio/src/components/TitleAnimation.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react';
+import { useSpring, animated } from 'react-spring';
+
+const TitleAnimation = ({text}) => {
+  const [gradientColors, setGradientColors] = useState(['#77b9f2', '#ed2b55', '#f5e1f3']);
+  
+  // Define the animation for the background color
+  const props = useSpring({
+    background: `linear-gradient(50deg, ${gradientColors[0]}, ${gradientColors[1]}, ${gradientColors[2]})`,
+    config: { duration: 3000 },
+  });
+
+  // Update gradient colors every 4 seconds (adjust as needed)
+  useEffect(() => {
+    const interval = setInterval(() => {
+      // Rotate the colors array to change the order of colors
+      setGradientColors((prevColors) => [
+        prevColors[1],
+        prevColors[2],
+        prevColors[0],
+
+      ]);
+    }, 3000); // Change colors every 4 seconds
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <>
+    <animated.div style={{ ...props, width: '20%', alignItems: 'center', textAlign: 'center', justifyContent: 'center', padding: '1em', borderRadius: '1.5em', margin: '1em'}}>
+      <h3>{text}</h3>
+    </animated.div>
+    </>
+  );
+};
+
+export default TitleAnimation;
